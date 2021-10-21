35afcbd5734e547599f0a0dee7538be4014ffe30
jamsh1dbe <jamshidbeksharipov2@gmail.com>
2021-10-21 20:45:41 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,138 @@
+// masala 1
+
+// var a = 4;
+// var p = a * 4;
+// console.log(p)
+
+// masala 2
+
+// var a = 6;
+// var s = 6 ** 2;
+// console.log(s)
+
+// masala 3
+
+// var a = 7;
+// var b = 14;
+// var p = 2 * (7 + 14)
+// console.log(p)
+// var s = 7 * 14
+// console.log (s)
+
+// masala 4
+
+// var d = 10;
+// var L = 3.14 * d;
+// console.log(L)
+
+// masala 5
+
+// var a = 5;
+// var V = a ** 3;
+// console.log(V)
+// var S = 6 * a ** 2;
+// console.log(S)
+
+// masala 6
+
+// var a = 3
+// var b = 4
+// var c = 5
+// var V = a * b * c
+// console.log(V)
+// var S = 2 * (a * b + b * c + a * c)
+// console.log(S)
+
+// masala 7
+
+// var R = 10
+// var p = 3.14
+// var L = 2 * p * R
+// console.log(L)
+
+// var S = p * R ** 2
+// console.log(S)
+
+// masala 8
+
+// var a = 5 
+// var b = 10
+// console.log((a + b) / 2)
+
+// masala 9 
+
+// var a = 5 
+// var b = 9
+// console.log((a * b) **0.5)
+
+// masala 10
+
+// var a = 6
+// var b =12
+// console.log(a + b)
+// console.log(a * b)
+// console.log(a ** 2)
+// console.log(b ** 2)
+
+// masala 11
+
+// var a = 4
+// var b =14
+// console.log(a + b)
+// console.log(a * b)
+// console.log(a)
+// console.log(b)
+
+// masala 12
+
+// var a = 4
+// var b =14
+// var c = ( a ** 2) + ( b ** 2)
+// console.log(c ** 0.5)
+
+// var P = a + b + c 
+// console.log(P)
+
+// masala 13
+
+// var R1 = 20
+// var R2 = 15
+// var p = 3.14
+// var S1 = p * R1
+// console.log( S1)
+// var S2 = p * R2
+// console.log( S2)
+// var S3 = p * (R1 - R2)
+// console.log( S3)
+
+//  masala 14
+
+// var L = 21
+// var p = 3.14
+// var R = L / (2 * p)
+// console.log(R)
+// var S = p * (R ** 2)
+// console.log(S)
+
+// masala 15
+ 
+// var S = 20
+// var p = 3.14
+// var R = ( S / p ) **0.5
+// console.log(R)
+// var d = 2 * R
+// console.log(d)
+
+// masala 16
+
+// var Z1 = -20
+// var Z2 = 27
+// console.log( Z2 - Z1)
+
+// masala 17 
+
+var A = 10
+var B = 20
+var C = 40
+console.log( C - A)
+console.log( C - B)
